21091f8ff4d4ae0109606f036727969f3349e6e1
Romain Gille <romain.gille@gmail.com>
2018-05-29 09:35:45 +0200

    improve run.sh with arguments

--- run.sh
+++ run.sh
@@ -1,33 +1,67 @@
 #!/bin/sh
 
+# change path to matlab bin
 MATLAB=/usr/local/MATLAB/bin/matlab
 EFAN=src/efan
 TV=src/tv
 IMG=../../img
 OUT=../../out
 
-echo "------------------------------"
-echo "-----Removing old outputs-----"
-echo "------------------------------"
-rm -v out/*
-cd $TV && make clean && cd ../..
+if [ $# -eq 0 ]; then
+    echo "-----------------------------------------------------------"
+    echo "You have to put one of the following arguments: "
+    echo "- all : run all the programs"
+    echo "- efan : run the EFAN program"
+    echo "- tv : run the TVINPAINT program"
+    echo "- clean : clean the project"
+    echo "----------------"
+    echo "You can also add a second argument as an input of programs"
+    echo "Just write the name of the file you have in the img/ folder"
+    echo "This option does not work for EFAN, you have to change the"
+    echo "input file in src/efan/demo.m file."
+    echo "------------------------------------------------------------"
+fi
 
-echo "------------------------------"
-echo "---------Running EFAN---------"
-echo "------------------------------"
 
-$MATLAB -nodisplay -nosplash -nodesktop -sd $EFAN -r "run('demo')"
+if [[ $1 == "clean" ]]; then
+    echo "---------------------------------------------"
+    echo "-----Removing old outputs and make clean-----"
+    echo "---------------------------------------------"
+    rm -v out/* img/mask.png img/masked.png
+    cd $TV && make clean && cd ../..
+fi
 
-echo "------------------------------"
-echo "---------Running TV-----------"
-echo "------------------------------"
+if [[ $1 == "efan" || $1 == "all" ]]; then
+    echo "---------------------------------------------"
+    echo "----------------Running EFAN-----------------"
+    echo "---------------------------------------------"
 
-cd $TV && make
+    # run the efan program with matlab and options specified in src/efan/demo.m
+    $MATLAB -nodisplay -nosplash -nodesktop -sd $EFAN -r "run('demo')"
+fi
 
-./randmask Bernoulli:0.01 $IMG/lena.png $IMG/mask.png
+if [[ $1 == "tv" || $1 == "all" ]]; then
+    echo "---------------------------------------------"
+    echo "----------------Running TV-------------------"
+    echo "---------------------------------------------"
 
-./applymask $IMG/lena.png $IMG/mask.png $IMG/masked.png
+    cd $TV && make -j
 
-./tvinpaint $IMG/mask.png 100 $IMG/masked.png $OUT/lena_tv.bmp
+    if [ $# -eq 2 ]; then
+        # create the random mask of pixels with 0.01% of the image
+        ./randmask Bernoulli:0.01 $IMG/$2 $IMG/mask.png
 
-cd ../..
+        # apply the mask on masked.png
+        ./applymask $IMG/$2 $IMG/mask.png $IMG/masked.png
+    else
+        # create the random mask of pixels with 0.01% of the image
+        ./randmask Bernoulli:0.01 $IMG/lena.png $IMG/mask.png
+
+        # apply the mask on masked.png
+        ./applymask $IMG/lena.png $IMG/mask.png $IMG/masked.png
+
+    fi
+    ./tvinpaint $IMG/mask.png 100 $IMG/masked.png $OUT/lena_tv.bmp
+
+    cd ../..
+fi
